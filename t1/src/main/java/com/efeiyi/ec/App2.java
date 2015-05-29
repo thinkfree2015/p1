9ac7f372fb7ad910e1b90d3b6673d6c74e502eaf
Hugh <2252327467@qq.com>
2015-05-29 15:01:10 +0800

    ...

--- t1/src/main/java/com/efeiyi/ec/App2.java
+++ t1/src/main/java/com/efeiyi/ec/App2.java
@@ -6,7 +6,13 @@ package com.efeiyi.ec;
  */
 public class App2
 {
+    public int jian(int a, int b){
+        return a - b;
+    }
 
+    public int chu(int a, int b){
+        return a / b;
+    }
 
     public static void main( String[] args )    {
         //Hello h = new com.efeiyi.ec.Hello();
--- t1/src/test/java/com/efeiyi/ec/AppTest.java
+++ t1/src/test/java/com/efeiyi/ec/AppTest.java
@@ -1,35 +1,29 @@
 package com.efeiyi.ec;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 
 /**
  * Unit test for simple App2.
  */
-public class AppTest
-        extends TestCase {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest(String testName) {
-        super(testName);
+public class AppTest {
+
+    private App2 a;
+
+    @Before
+    public void setUp(){
+        a = new App2();
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite() {
-        System.out.print("testing====================================================================");
-        return new TestSuite(AppTest.class);
+    @Test
+    public void testJian(){
+        Assert.assertEquals("wrong",1,a.jian(3,2));
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp() {
-        assertTrue(true);
+    @Test
+    public void testChu(){
+        Assert.assertEquals("wrong",3,a.chu(4,2));
     }
 }
